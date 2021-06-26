dc7e304a7530d95101cec51766f7fcb501c20b83
cheeeeze <nyj9349@hanmail.net>
2021-06-26 17:59:50 +0900

    Add new files

---
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -0,0 +1,21 @@
+package chap02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main( String[] args ) {
+		
+		AnnotationConfigApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(AppContext.class);
+		
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter2", Greeter.class);
+		String msg = g1.greet("스프링");
+		String msg2 = g2.greet("스프링");
+		System.out.println(" g1 === g2 : " + ( g1 == g2) );
+		System.out.println(msg);
+		System.out.println(msg2);
+		ctx.close();
+	}
+}
