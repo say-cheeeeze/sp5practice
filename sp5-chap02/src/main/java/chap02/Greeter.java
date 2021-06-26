838fdc454ed389fa64948b0091e217a17f28451e
cheeeeze <nyj9349@hanmail.net>
2021-06-26 17:58:55 +0900

    Modify Greeter.java

--- sp5-chap02/src/main/java/chap02/Greeter.java
+++ sp5-chap02/src/main/java/chap02/Greeter.java
@@ -4,4 +4,12 @@ public class Greeter {
 
 	private String format;
 	
+	public String greet( String guest ) {
+		return String.format( format, guest );
+	}
+	
+	public void setFormat( String format ) {
+		this.format = format;
+	}
+		
 }
