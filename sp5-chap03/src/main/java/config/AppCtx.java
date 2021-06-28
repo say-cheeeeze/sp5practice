c7ce57a9f328c9aa800dfb77140a6858b5927d5c
cheeeeze <nyj9349@hanmail.net>
2021-06-28 21:54:21 +0900

    Add Spring configuration

---
+++ sp5-chap03/src/main/java/config/AppCtx.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+
+@Configuration
+public class AppCtx {
+	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegService() {
+		return new MemberRegisterService( memberDao() );
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdService() {
+		ChangePasswordService passwordService = new ChangePasswordService();
+		passwordService.setMemberDao(memberDao());
+		return passwordService;
+	}
+
+}
---
+++ sp5-chap03/src/main/java/main/MainForSpring.java
@@ -0,0 +1,117 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.AppCtx;
+import spring.ChangePasswordService;
+import spring.DuplicateMemberException;
+import spring.MemberListService;
+import spring.MemberNotFoundException;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
+import spring.WrongIdPasswordException;
+
+/** 
+* 	@packageName	:	main 
+* 	@fileName		:	MainForSpring.java 
+* 	@author			:	cheeeeze
+* 	@date			:	2021.06.28 
+* 	@description	:	스프링 빈 객체 생성 및 의존 주입역할	 
+* =========================================================== 
+* 	DATE 			AUTHOR 					NOTE 
+* ----------------------------------------------------------- 
+* 	2021.06.28 		cheeeeze 				최초 생성 
+*/
+public class MainForSpring {
+
+	// 앞서 Assembler클래스를 통해 작성한 클래스를 스프링 컨테이너를 사용하도록 변경함
+	
+	private static ApplicationContext ctx = null;
+	
+	public static void main( String[] args ) throws IOException {
+		
+		ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+		
+		BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+		
+		while( true ) {
+			
+			System.out.println( "명령어를 입력하세요.");
+			String command = reader.readLine();
+			
+			if( command.equalsIgnoreCase("exit") ) {
+				System.out.println("종료합니다...");
+				break;
+			}
+			
+			// readLine() 으로 입력받은 문자열은 " " 공백 한칸을 기준으로 배열을 return 한다.
+			// 이 배열을 가지고 각기 다른 command 을 요청하게 된다.
+			
+			if( command.startsWith( "new ") ) {
+				processNewCommand( command.split(" ") );
+				continue;
+			}
+			else if( command.startsWith( "change " ) ) {
+				processChangeCommand( command.split(" ") );
+				continue;
+			}
+			else if ( command.equals( "list" ) ) {
+				processShowList();
+				continue;
+			}
+			printWrong();
+			
+		}
+		
+		
+	}
+	/**
+	 * 회원 등록 처리합니다.
+	 */
+	private static void processNewCommand( String[] args ) {
+		
+		if( args.length != 5 ) {
+			printWrong();
+			return;
+		}
+		
+//		MemberRegisterService regService = ctx.getBean(null)
+		
+	}
+	
+	/**
+	 * 회원 정보 변경합니다.
+	 */
+	private static void processChangeCommand( String[] args ) {
+	}
+	
+	/**
+	 * 회원 목록을 보여줍니다. 
+	 */
+	private static void processShowList() {
+	}
+	
+
+	private static void printWrong() {
+		System.out.println();
+		System.out.println("잘못된 명령입니다. 아래 사용법을 확인하세요.");
+		printHelp();
+	}
+	
+	private static void printHelp() {
+		System.out.println();
+		System.out.println("각 인자값은 띄어쓰기 한칸으로 구분하여 입력합니다.");
+		System.out.println("등록인 경우 : new 이메일 이름 암호 암호확인 ");
+		System.out.println("변경인 경우 : change 이메일 현재암호 변경할암호");
+		System.out.println("회원 목록 조회 : list");
+		System.out.println("프로그램 종료 : exit");
+		System.out.println();
+		
+	}
+	
+}
